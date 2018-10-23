808dca3e4bd443112d865bdb85455f14b69e185b
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2018-10-23 19:19:58 -0400

    so close and yet so far

---
+++ client/build/precache-manifest.46a8dbb7974eea2181ef5f3187082128.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d7a27bec85445deee10d",
+    "url": "/static/js/main.d7a27bec.chunk.js"
+  },
+  {
+    "revision": "3a1b3c56196bf7c3fb57",
+    "url": "/static/js/1.3a1b3c56.chunk.js"
+  },
+  {
+    "revision": "d7a27bec85445deee10d",
+    "url": "/static/css/main.2f2e6182.chunk.css"
+  },
+  {
+    "revision": "50c9db5e6a32613d5b9a7e9446353f98",
+    "url": "/index.html"
+  }
+];
---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import ListsContainer from "./components/ListsContainer";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ListsContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
